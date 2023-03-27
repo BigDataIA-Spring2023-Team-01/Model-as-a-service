20f05ea8ed90998c3c3b844c46272f410f89058c
Anandita <deb.ana@northeastern.edu>
2023-03-27 15:31:37 -0400

    streamlit

--- streamlit/main.py
+++ streamlit/main.py
@@ -1,49 +1,69 @@
-
-# from openapi import uploadTos3
-
 import streamlit as st
 import pandas as pd
 from io import StringIO
 import os
-# from dotenv import load_dotenv
-# from dotenv import load_dotenv
 import boto3
 import sys
-
-# @ -24,6 +27,7 @@ processedTranscriptBucket = os.environ.get('processedTranscriptBucket')
+import io
 
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-# load_dotenv()
 
 
-# @ -37,7 +41,46 @@ if uploaded_file is not None:
-
-#     st.success('File Upload Successful')
-    # uploadTos3(uploaded_file,raws3Bucket,'samplemp3')
-# load_dotenv()
-# raws3Bucket = os.environ.get('raws3Bucket')
-
-# uploaded_file = st.file_uploader("Choose a file")
-# if uploaded_file is not None:
-
-#     uploadTos3(uploaded_file,raws3Bucket,'samplemp3')
 st.header("Model As A Service")
-# col1, col2 = st.beta_columns(2)
 
+audio_file = st.file_uploader("Attach an audio file", type = 'mp3', on_change = None)
 
-# with col1:
-# col3, col4 = st.columns(2)
-# with col3:
-st.file_uploader("Attach an audio file", type = 'mp3', accept_multiple_files = True, on_change = None)
-
-# with col4:
 st.button("Upload Button")
+# if audio_file:
+#     st.write(audio_file.name)
+
+# st.set_option('deprecation.showfileUploaderEncoding', False)
+def upload_to_s3(file, bucket_name):
+    s3 = boto3.resource('s3')
+    s3.Bucket(bucket_name).put_object(Key=file.name, Body=file)
+# def upload_to_s3(file, bucket_name, key):
+#     s3.upload_fileobj(file, bucket_name, key)
+
+# # Create S3 client
+# s3 = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+
+# # if uploaded_file is not None:
+    
+
+# #     # Read uploaded file as bytes
+# #     file_bytes = uploaded_file.read()
+
+# #     # Create BytesIO object from file bytes
+# #     audio_file = [io.BytesIO(file_bytes)]
+
+# #     # Set name attribute of BytesIO object
+# #     # audio_file[0].name = f"{file_name}.mp3"
+
+# #     # Upload file to S3
+# #     bucket_name = "rawmp3"
+# #     key = f"{file_name}.mp3"
+# #     upload_to_s3(audio_file[0], bucket_name, key)
+
+# #     # Display success message to user
+# #     st.success(f"{file_name}.mp3 was uploaded to S3!")
+
+# Check if a file was uploaded
+if audio_file is not None:
+    # Extract file name from uploaded file
+    # file_name = st.text_input("Enter filename for ")
+    # Upload the file to S3 bucket
+    upload_to_s3(audio_file, BUCKET_NAME)
+    # Rename the file with .mp3 extension
+    os.rename(file_name, file_name + ".mp3")
+    st.write("File uploaded successfully!")
+else:
+    st.write("Please upload an MP3 file.")
 
 
 st.markdown("-------")
 
-# with col2:
+
 options = ['Processed Audio List', 'Recording_2', 'Recording_3']
 selected_option = st.selectbox("Select Processed Audio from the list", options)
 
