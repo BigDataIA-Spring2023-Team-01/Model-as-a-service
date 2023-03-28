9f863b9a621a4f8ed22eee854b1ada57303bb83a
Anandita <deb.ana@northeastern.edu>
2023-03-28 12:37:47 -0400

    streamlit

--- streamlit/main.py
+++ streamlit/main.py
@@ -5,60 +5,71 @@ import os
 import boto3
 import sys
 import io
+import requests
 
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
+s3client = boto3.client('s3',region_name='us-east-1',
+                        aws_access_key_id = AWS_ACCESS_KEY_ID,
+                        aws_secret_access_key = AWS_SECRET_ACCESS_KEY)
 
 st.header("Model As A Service")
 
-audio_file = st.file_uploader("Attach an audio file", type = 'mp3', on_change = None)
+audio_file = st.file_uploader("Attach an audio file", type = 'mp3')
 
-st.button("Upload Button")
-# if audio_file:
-#     st.write(audio_file.name)
-
-# st.set_option('deprecation.showfileUploaderEncoding', False)
-def upload_to_s3(file, bucket_name):
-    s3 = boto3.resource('s3')
-    s3.Bucket(bucket_name).put_object(Key=file.name, Body=file)
-# def upload_to_s3(file, bucket_name, key):
-#     s3.upload_fileobj(file, bucket_name, key)
-
-# # Create S3 client
-# s3 = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
-
-# # if uploaded_file is not None:
-    
-
-# #     # Read uploaded file as bytes
-# #     file_bytes = uploaded_file.read()
-
-# #     # Create BytesIO object from file bytes
-# #     audio_file = [io.BytesIO(file_bytes)]
-
-# #     # Set name attribute of BytesIO object
-# #     # audio_file[0].name = f"{file_name}.mp3"
-
-# #     # Upload file to S3
-# #     bucket_name = "rawmp3"
-# #     key = f"{file_name}.mp3"
-# #     upload_to_s3(audio_file[0], bucket_name, key)
-
-# #     # Display success message to user
-# #     st.success(f"{file_name}.mp3 was uploaded to S3!")
 
 # Check if a file was uploaded
 if audio_file is not None:
-    # Extract file name from uploaded file
-    # file_name = st.text_input("Enter filename for ")
-    # Upload the file to S3 bucket
-    upload_to_s3(audio_file, BUCKET_NAME)
-    # Rename the file with .mp3 extension
-    os.rename(file_name, file_name + ".mp3")
-    st.write("File uploaded successfully!")
-else:
-    st.write("Please upload an MP3 file.")
+    
+    if st.button("Upload Button"):
+        s3client.upload_fileobj(audio_file, USER_BUCKET_NAME, audio_file.name)
+        st.success("File uploaded successfully!", icon="✅")
+    else:
+        st.write("Please upload an MP3 file.")
+
+def whisper_api(question, transcript_url):
+    # Define the API endpoint and headers
+    endpoint = "https://api.whisper.ai/v1/ask"
+    headers = {"Authorization": "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"}
+
+    # Define the request payload
+    data = {
+        "question": question,
+        "audio_url": transcript_url
+    }
+
+    # Call the API
+    response = requests.post(endpoint, headers=headers, json=data)
+
+    # Return the response
+    return response.json()["answer"]
+
+def main():
+    # Create a file uploader in Streamlit
+    # uploaded_file = st.file_uploader("Upload an audio file", type=["mp3", "wav"])
+    question = st.text_input("Ask a question")
+
+    # If a file is uploaded by the user and a question is entered
+    if audio_file is not None and question != "":
+
+        # Get the URL of the uploaded file
+        transcript_url = f"https://s3.amazonaws.com/processedtranscript/{audio_file.name}"
+
+        # Call the Whisper API to generate a response to the question
+        response = whisper_api(question, transcript_url)
+
+        # Display the response to the user
+        st.write(f"Question: {question}")
+        st.write(f"Response: {response}")
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
 
 
 st.markdown("-------")
