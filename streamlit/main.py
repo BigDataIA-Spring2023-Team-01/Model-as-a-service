521d961f384f8ae3ef807933c35bf4eb5b86721e
Anandita <deb.ana@northeastern.edu>
2023-03-29 17:29:41 -0400

    streamlit part 2 b

--- streamlit/main.py
+++ streamlit/main.py
@@ -8,12 +8,16 @@ import io
 import requests
 import json
 # from airflow import batch_dag
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-# load_dotenv()
+load_dotenv()
 USER_BUCKET_NAME = os.environ.get("raws3Bucket")
-
+# Set AWS credentials
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+# USER_BUCKET_NAME_MP3 = 'rawmp3'
+token = os.environ.get("token")
 
 
 s3client = boto3.client('s3',region_name='us-east-1',
@@ -33,6 +37,42 @@ if audio_file is not None:
         st.success("File uploaded successfully!", icon="✅")
     else:
         st.write("Please upload an MP3 file.")
+ 
+    
+st.markdown("-------")
+
+s3 = boto3.resource('s3', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+buckets3 = s3.Bucket('processedtranscript')
+file_names = [obj.key for obj in buckets3.objects.all()]
+selected_file = st.selectbox('Select a file', file_names)
+
+
+if selected_file:
+    s3 = boto3.resource('s3',aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+    bucket_name_ = 'chatgptresults'
+    bucket = s3.Bucket(bucket_name_)
+
+    file_name = selected_file
+    
+    try:
+        obj = s3.Object(bucket_name_, file_name)
+        contents = obj.get()['Body'].read().decode('utf-8')
+        data = json.loads(contents)
+        for key, value in data.items():
+            st.markdown(f"<p style='font-weight: bold;'>{key}: </p><p>{value}</p>", unsafe_allow_html=True)
+        #st.write(data)
+    except:
+        st.error("Failed to read file from S3 bucket.")
+    st.text_area('Generic Transcript Questionnaire',
+                 '''
+    1. What was the meeting about? 
+    2. How many people participated?
+    3. What was the conclusion of the discussion''', on_change = None)
+    
+
+
+    # st.button("Ask Button")
+
 
 def ask_question(question, context):
     headers = {
@@ -67,52 +107,15 @@ def ask_question(question, context):
     answer = response.json()["choices"][0]["message"]["content"].strip()
     return answer
 
-transcript_url = f"https://processedtranscript.s3.amazonaws.com/Promotion+Conversation"
+bucket_name = 'processedtranscript'
+file_url = 'https://s3.console.aws.amazon.com/s3/object/{}?region=us-east-1&prefix={}'.format(bucket_name,file_name)
+
+s3_object = s3client.get_object(Bucket=bucket_name, Key=file_name)
+file_content = s3_object['Body'].read().decode('utf-8')
+
+# print(file_content)
+
 question = st.text_input("Ask a question")
 if st.button("Ask Button"):
-    answer = ask_question(transcript_url, question)
+    answer = ask_question(file_content, question)
     st.text_area('Answer to question asked:', value = answer)
-    # st.write(answer)   
-
- 
-    
-st.markdown("-------")
-
-s3 = boto3.resource('s3', aws_access_key_id=os.environ.get('AWS_ACCESS_KEY'), aws_secret_access_key=os.environ.get('AWS_SECRET_KEY'))
-buckets3 = s3.Bucket('processedtranscript')
-file_names = [obj.key for obj in buckets3.objects.all()]
-selected_file = st.selectbox('Select a file', file_names)
-
-
-if selected_file:
-    s3 = boto3.resource('s3',aws_access_key_id=os.environ.get('AWS_ACCESS_KEY'), aws_secret_access_key=os.environ.get('AWS_SECRET_KEY'))
-    bucket_name_ = 'chatgptresults'
-    bucket = s3.Bucket(bucket_name_)
-
-    # Select JSON file to read
-    file_name = selected_file
-    
-    # Read JSON file from S3
-    try:
-        obj = s3.Object(bucket_name_, file_name)
-        contents = obj.get()['Body'].read().decode('utf-8')
-        data = json.loads(contents)
-        for key, value in data.items():
-            st.markdown(f"<p style='font-weight: bold;'>{key}: </p><p>{value}</p>", unsafe_allow_html=True)
-        #st.write(data)
-    except:
-        st.error("Failed to read file from S3 bucket.")
-    st.text_area('Generic Transcript Questionnaire',
-                 '''
-    1. What was the meeting about? 
-    2. How many people participated?
-    3. What was the conclusion of the discussion''', on_change = None)
-    
-
-    # st.text_input('Enter a question to answer related to the meeting', on_change = None)
-
-
-    # st.button("Ask Button")
-
-
-
