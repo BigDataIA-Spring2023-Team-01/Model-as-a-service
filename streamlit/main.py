d1e5d0e2d5e9a3bb6d455b5d421f9be6818d3f29
CY2404 <114796225+CY2404@users.noreply.github.com>
2023-03-31 00:11:05 -0400

    ui changes

--- streamlit/main.py
+++ streamlit/main.py
@@ -55,7 +55,7 @@ def background(png_file):
     ''' % bin_str
     st.markdown(page_bg_img, unsafe_allow_html=True)
 
-background(r"C:\Users\user\OneDrive\Desktop\DAMG_7245\Model-as-a-service\streamlit\download.jpeg")
+background(r"./data/download.jpeg")
 #File Uploader
 audio_file = st.file_uploader("Attach an audio file", type = 'mp3')
 
